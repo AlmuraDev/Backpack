9bef1fc9de4b58b8b4717c2109d941f45e1e83f7
Steven Downer <grinch@outlook.com>
2016-03-08 21:59:46 -0600

    Update Storage for TypeToken usage
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/com/almuradev/backpack/util/Storage.java
+++ src/main/java/com/almuradev/backpack/util/Storage.java
@@ -26,9 +26,10 @@ package com.almuradev.backpack.util;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Queues;
+import com.google.common.reflect.TypeToken;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
+import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.api.plugin.PluginContainer;
@@ -36,16 +37,14 @@ import org.spongepowered.api.plugin.PluginContainer;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class Storage {
 
     private ConfigurationLoader<CommentedConfigurationNode> loader;
     private CommentedConfigurationNode rootNode;
     private final File configuration;
-    private final List<CommentedNode> defaultNodes = Lists.newArrayList();
+    private final List<DefaultNode> defaultNodes = Lists.newArrayList();
     private final Logger logger;
 
     public Storage(PluginContainer container, File configuration, ConfigurationLoader<CommentedConfigurationNode> loader) {
@@ -79,21 +78,23 @@ public class Storage {
      * Loads the configuration file.
      * @return {@link Storage} for chaining.
      */
+    @SuppressWarnings("unchecked")
     public Storage load() {
         defaultNodes.stream().filter(entry -> entry.value != null).forEach(entry -> {
             final CommentedConfigurationNode node = getChildNode(entry.key);
             if (node.getValue() == null) {
-                getChildNode(entry.key).setValue(entry.value).setComment(entry.comment);
+                if (entry.token.isPresent()) {
+                    try {
+                        getChildNode(entry.key).setValue((TypeToken) entry.token.get(), entry.value);
+                        getChildNode(entry.key).setComment(entry.comment);
+                    } catch (ObjectMappingException e) {
+                        logger.warn("Unable to map TypeToken!", e);
+                    }
+                } else {
+                    getChildNode(entry.key).setValue(entry.value).setComment(entry.comment);
+                }
             }
         });
-        final Queue<CommentedConfigurationNode> queue = Queues.newConcurrentLinkedQueue();
-        queue.add(rootNode);
-        while (!queue.isEmpty()) {
-            final CommentedConfigurationNode node = queue.remove();
-            if (node.hasMapChildren()) {
-                queue.addAll(node.getChildrenMap().entrySet().stream().map(Map.Entry::getValue).collect(Collectors.toList()));
-            }
-        }
         save();
         try {
             loader.load();
@@ -117,12 +118,22 @@ public class Storage {
     }
 
     /**
-     * Registers a default node. See {@link Storage#registerDefaultNode(String, Object)}.
+     * Registers a default node. See {@link Storage#registerDefaultNode(String, Object, String)}.
      * @param key The key to register.
      * @param value The value to register.
      */
     public void registerDefaultNode(String key, Object value) {
         this.registerDefaultNode(key, value, null);
+    }
+
+    /**
+     * Registers a default node without a {@link TypeToken}. See {@link Storage#registerDefaultNode(String, Object, String, Optional)}.
+     * @param key The key to register.
+     * @param value The value to register.
+     * @param comment The comment to register.
+     */
+    public void registerDefaultNode(String key, Object value, String comment) {
+        registerDefaultNode(key, value, comment, Optional.empty());
     }
 
     /**
@@ -136,17 +147,19 @@ public class Storage {
      * }</p>
      * @param value The value to register.
      * @param comment The comment to register.
+     * @param token The optional {@link TypeToken} for the value being registered.
      */
-    public void registerDefaultNode(String key, Object value, String comment) {
+    @SuppressWarnings("unchecked")
+    public <T> void registerDefaultNode(String key, Object value, String comment, Optional<TypeToken<T>> token) {
         final String[] nodes = key.split("\\.");
         final List<String> currentPath = Lists.newArrayList();
         for (int i = 0; i < nodes.length; i++) {
             if (i < nodes.length - 1) {
                 currentPath.add(i, nodes[i]);
                 final String joinedPath = Joiner.on(",").skipNulls().join(currentPath).replace(",", ".");
-                defaultNodes.add(new CommentedNode(joinedPath, null, null));
+                defaultNodes.add(new DefaultNode(joinedPath, null, null, token));
             } else {
-                defaultNodes.add(new CommentedNode(key, value, comment));
+                defaultNodes.add(new DefaultNode(key, value, comment, token));
             }
         }
         this.save();
@@ -162,16 +175,33 @@ public class Storage {
         return rootNode.getNode((Object[]) path.split("\\."));
     }
 
-    public static class CommentedNode {
+    public Object getChildValue(String path) {
+        return getChildNode(path).getValue();
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getChildValue(String path, Class<T> clazz) {
+        try {
+            return getChildNode(path).getValue(TypeToken.of(clazz));
+        } catch (ObjectMappingException e) {
+            logger.error("Unable to map object to TypeToken", e);
+        }
+        // Return a value even if the token doesn't successfully map
+        return (T) getChildNode(path).getValue();
+    }
+
+    public static class DefaultNode<T> {
 
         public String key;
-        public Object value;
+        public T value;
         public String comment;
+        public final Optional<TypeToken<T>> token;
 
-        public CommentedNode(String key, Object value, String comment) {
+        public DefaultNode(String key, T value, String comment, Optional<TypeToken<T>> token) {
             this.key = key;
             this.value = value;
             this.comment = comment;
+            this.token = token;
         }
     }
 }
